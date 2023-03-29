3f4369b2f822697875f7a422fcae261eadbf1728
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-29 18:25:05 -0500

    agregue el componente formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -3,7 +3,43 @@ import Header from './Header';
 
 const Formulario = () => {
     return (
-        <h1>Formulario aqui</h1>
+        <form
+            className='col-12'
+        >
+            <fieldset className='text-center'>
+                <legend>
+                    buscar bebidas por categoria o ingredientes
+                </legend>
+            </fieldset>
+
+            <div className='row mt-4'>
+                <div className='col-md-4'>
+                    <input
+                        name='nombre'
+                        className='form-control'
+                        type='text'
+                        placeholder='Buscar por ingrediente'
+                    />
+                </div>
+                <div className='col-md-4'>
+                    <select
+                        className='form-control'
+                        name='categoria'
+                    >
+                        <option value="">-- Selecciona Categoria</option>
+                    </select>
+                </div>
+
+                <div className='col-md-4'>
+                    <input  
+                    type="submit" 
+                    className='btn btn-block btn-primary'
+                    name="" 
+                    value="Buscar Bebidas"
+                    />
+                </div>
+            </div>
+        </form>
 
     );
 }
