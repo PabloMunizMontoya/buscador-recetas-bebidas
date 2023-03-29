e9968478b30c7dcc84d0bb10049e9f0dfdb5090a
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-29 17:45:14 -0500

    primer comit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# buscador-recetas-bebidas
+buscador de reteas de bebidas usando context para consumir api
---
+++ src/components/Formulario.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+import Header from './Header';
+
+const Formulario = () => {
+    return (
+        <h1>Formulario aqui</h1>
+
+    );
+}
+
+export default Formulario;
