89ff60a324e77c6807f143544b7aa37e11ad94ea
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-29 22:03:31 -0500

    guarde los datos de el input text y la categoria seleccionada en un useState con un onChange aplicado al formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,11 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { CategoriasContext } from '../context/CategoriasContext'
 
 
 const Formulario = () => {
 
+    const [busqueda, guardarBusqueda] = useState({
+        nombre: '',
+        categoria: ''
+    })
+
+    const obtenerDatos = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    }
+
     //1.6 usamos el nuevo hook para hacer funcionar context, este hook usa la variable que contiene la function createContext(), de esta forma ya tendremos disponible todo lo que tiene el value context dentro del provider.
     const {categorias} = useContext( CategoriasContext)
+
+
 
     console.log(categorias)
 
@@ -26,12 +40,14 @@ const Formulario = () => {
                         className='form-control'
                         type='text'
                         placeholder='Buscar por ingrediente'
+                        onChange={obtenerDatos}
                     />
                 </div>
                 <div className='col-md-4'>
                     <select
                         className='form-control'
                         name='categoria'
+                        onChange={obtenerDatos}
                     >
                         <option value="">-- Selecciona Categoria</option>
                         {categorias.map(categoria => (
