402a02709fb7f9444a3c44fb7160c081e687543a
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-04-01 15:52:35 -0500

    use un context para guardar la receta obteniendo el id desde el componente receta

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,10 +1,27 @@
 import React, {createContext, useEffect,useState} from 'react'
+import Axios from 'axios'
 
 export const ModalContext = createContext()
 
 const ModalProvider = (props) => {
 
     const [idReceta, guardarIdReceta] = useState(null)
+    const [ receta, guardarRecetas] = useState({})
+
+    useEffect (() => {
+        const obtenerReceta = async () => {
+
+            if(!idReceta) return
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceta}`
+
+            const resultado = await Axios.get(url)
+            console.log(resultado.data.drinks[0])
+            /* guardarRecetas(recetas) */
+        }
+        obtenerReceta()
+    },[idReceta]) 
+
     return ( 
         <ModalContext.Provider
 
